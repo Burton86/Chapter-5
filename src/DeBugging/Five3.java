d3a6a02290831241e4211f859b0325fe0f4ac345
Burton86 <kyleb286@gmail.com>
2019-10-01 08:44:27 -0500

    update

--- src/DeBugging/Five3.java
+++ src/DeBugging/Five3.java
@@ -18,13 +18,15 @@ public class Five3 {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter item number");
         item = input.nextInt();
-        if(item < LOW) output = "Item number too low";
-        else
-        if(item > HIGH)
+        if(item < LOW) {
+            output = "Item number too low";
+        }
+        else if(item > HIGH) {
             output = "Item number too high";
-        else
-        if(item > CUTOFF)
+        }
+        else if(item > CUTOFF) {
             output = "Valid - in Automotive Department";
+        }
         else
             output = "Valid - Item in Housewares Department";
         System.out.println(output);
